1b30274e3109db2951c5512769401f7fb38887ed
vishwajeet-wlc <vishwajeet@wlc.technology>
2024-11-06 10:48:25 +0530

    Admissions and programs pages.

---
+++ src/scripts/components/admission/admission.js
@@ -0,0 +1,27 @@
+import { renderContactUsScreen } from "../common.js";
+import {
+	renderAdmissionIntoScreen,
+	renderAdmissionFeeStructureScreen,
+	renderAdmissionProcessScreen1,
+	renderAdmissionProcessScreen2,
+	renderAdmissionRegistrationScreen1,
+	renderAdmissionRegistrationScreen2,
+} from "./admission-components.js";
+
+document.addEventListener("DOMContentLoaded", () => {
+	const admissionContainer = document.getElementById("admission-1");
+	const admissionContainer2 = document.getElementById("admission-2");
+	const admissionContainer3 = document.getElementById("admission-3");
+	const admissionContainer4 = document.getElementById("admission-4");
+	const admissionContainer5 = document.getElementById("admission-5");
+	const admissionContainer6 = document.getElementById("admission-6");
+	const admissionContainer7 = document.getElementById("admission-7");
+
+	admissionContainer.innerHTML += renderAdmissionIntoScreen();
+	admissionContainer2.innerHTML += renderAdmissionProcessScreen1();
+	admissionContainer3.innerHTML += renderAdmissionProcessScreen2();
+	admissionContainer4.innerHTML += renderAdmissionFeeStructureScreen();
+	admissionContainer5.innerHTML += renderAdmissionRegistrationScreen1();
+	admissionContainer6.innerHTML += renderAdmissionRegistrationScreen2();
+	admissionContainer7.innerHTML += renderContactUsScreen();
+});
--- src/scripts/responsiveVideo.js
+++ src/scripts/responsiveVideo.js
@@ -1,12 +1,16 @@
-// / for video player
-const videoSource = document.getElementById("video-source");
 function updateVideoSource() {
-  if (window.innerWidth <= 768) {
-    videoSource.src = "./src/assessts/videos/vertical.mp4";
-  } else {
-    videoSource.src = "./src/assessts/videos/horizontal.mp4";
-  }
-  document.getElementById("responsive-video").load(); //some time it the source is updating the source and some time not. So dont remove it shivame sir
+	const videoSource = document.getElementById("video-source");
+
+	if (window.innerWidth <= 768) {
+		videoSource.setAttribute("src", "./src/assessts/videos/vertical.mp4");
+	} else {
+		videoSource.setAttribute("src", "./src/assessts/videos/horizontal.mp4");
+	}
+	document.getElementById("responsive-video").load(); //some time it the source is updating the source and some time not. So dont remove it shivame sir
 }
-updateVideoSource();
-window.addEventListener("resize", updateVideoSource);
+
+document.addEventListener("DOMContentLoaded", (event) => {
+	updateVideoSource();
+	event.preventDefault();
+	window.addEventListener("resize", updateVideoSource);
+});
