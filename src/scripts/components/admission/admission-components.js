979cb668aa2646f009259fde62239ce650527d83
shivam-wlc <shivam@wlc.technology>
2024-11-08 18:15:03 +0530

    Scrolling left right fixed

--- src/scripts/components/admission/admission-components.js
+++ src/scripts/components/admission/admission-components.js
@@ -1,5 +1,5 @@
 function renderAdmissionIntoScreen() {
-	return `
+  return `
         <div class="admission-semi-circle-1">
           <div class="admission-content">
               <button>ADMISSIONS</button>
@@ -42,7 +42,7 @@ function renderAdmissionIntoScreen() {
 }
 
 function renderAdmissionProcessScreen1() {
-	return `
+  return `
         <div class="admission-semi-circle-2">
           <div class="admission-content">
             <button>ADMISSIONS PROCESS</button>
@@ -72,7 +72,7 @@ function renderAdmissionProcessScreen1() {
       `;
 }
 function renderAdmissionProcessScreen2() {
-	return `
+  return `
         <div class="admission-semi-circle-3">
           <div class="admission-content">
           </br></br></br>
@@ -97,7 +97,7 @@ function renderAdmissionProcessScreen2() {
 }
 
 function renderAdmissionFeeStructureScreen() {
-	return `
+  return `
         <div class="admission-semi-circle-4">
           <div class="admission-content">
             <button>FEE STRUCTURE</button>
@@ -120,76 +120,126 @@ function renderAdmissionFeeStructureScreen() {
       `;
 }
 
+// function renderAdmissionRegistrationScreen1() {
+// 	const width = window.innerWidth;
+// 	return `
+//         <div class="admission-semi-circle-5">
+//           <div class="admission-content">
+//               <div class="registration-form-container-1">
+//                    <div class="input-container">
+//                       <label for="firstName"> Child First Name: </label>
+//                       <input type="text" name="first-name"/>
+//                    </div>
+//                    <div class="input-container">
+//                       <label for="lastName"> Child Last Name: </label>
+//                       <input type="text" name="last-name"/>
+//                    </div>
+//                    <div class="input-container">
+//                       <label for="dob"> Child DOB: </label>
+//                       <input type="date" name="dob"/>
+//                    </div>
+//                     <div class="input-container">
+//                       <label for="father's name"> Father's Name: </label>
+//                       <input type="text" name="father-name"/>
+//                    </div>
+//                    <div class="input-container">
+//                       <label for="mother's name"> Mother's Name: </label>
+//                       <input type="text" name="mother-name"/>
+//                    </div>
+//                    <div class="input-container">
+//                       <label for="mobile"> Mobile Number: </label>
+//                       <input type="text" name="mobile"/>
+//                    </div>
+//                    <div class="input-container">
+//                       <label for="email"> Email ID: </label>
+//                       <input type="email" name="email"/>
+//                    </div>
+//                     <div class="input-container">
+//                       <label for="program"> Select Programme: </label>
+//                       <div class="radio-options-container">
+//                         <div class="radio-option">
+//                           <input type="radio" name="program" value="playgroup"/>
+//                           <label for="playgroup"> Play Group (16-30 months) <br/> November 2024 Batch </label>
+//                         </div>
+//                         <div class="radio-option">
+//                           <input type="radio" name="program" value="nursery"/>
+//                           <label for="nursery"> Nursery (2.5-3.5 years) <br/> November 2024 Batch </label>
+//                         </div>
+//                         <div class="radio-option" >
+//                           <input type="radio" name="program" value="jrkg"/>
+//                           <label for="jrkg"> Jr.KG (3.5-4.5 years) <br/> June 2025 Batch </label>
+//                         </div>
+//                         <div class="radio-option">
+//                           <input type="radio" name="program" value="srkg"/>
+//                           <label for="srkg"> Sr.KG (4.5-5.5 years) <br/> June 2025 Batch </label>
+//                         </div>
+//                       </div>
+//                    </div>
+//                    <div class="button-container">
+//                       <button>Cancel</button>
+//                       <button>Submit</button>
+//                    </div>
+//               </div>
+//           </div>
+//         </div>
+//       `;
+// }
+
 function renderAdmissionRegistrationScreen1() {
-	const width = window.innerWidth;
-	return `
-        <div class="admission-semi-circle-5">
-          <div class="admission-content">
-              <div class="registration-form-container-1">
-                   <div class="input-container">
-                      <label for="firstName"> Child First Name: </label>
-                      <input type="text" name="first-name"/>
-                   </div>
-                   <div class="input-container">
-                      <label for="lastName"> Child Last Name: </label>
-                      <input type="text" name="last-name"/>
-                   </div>
-                   <div class="input-container">
-                      <label for="dob"> Child DOB: </label>
-                      <input type="date" name="dob"/>
-                   </div>
-                    <div class="input-container">
-                      <label for="father's name"> Father's Name: </label>
-                      <input type="text" name="father-name"/>
-                   </div>
-                   <div class="input-container">
-                      <label for="mother's name"> Mother's Name: </label>
-                      <input type="text" name="mother-name"/>
-                   </div>
-                   <div class="input-container">
-                      <label for="mobile"> Mobile Number: </label>
-                      <input type="text" name="mobile"/>
-                   </div>
-                   <div class="input-container">
-                      <label for="email"> Email ID: </label>
-                      <input type="email" name="email"/>
-                   </div>
-                    <div class="input-container">
-                      <label for="program"> Select Programme: </label>
-                      <div class="radio-options-container">
-                        <div class="radio-option">
-                          <input type="radio" name="program" value="playgroup"/>
-                          <label for="playgroup"> Play Group (16-30 months) <br/> November 2024 Batch </label>
-                        </div>
-                        <div class="radio-option">
-                          <input type="radio" name="program" value="nursery"/>
-                          <label for="nursery"> Nursery (2.5-3.5 years) <br/> November 2024 Batch </label>
-                        </div>
-                        <div class="radio-option" >
-                          <input type="radio" name="program" value="jrkg"/>
-                          <label for="jrkg"> Jr.KG (3.5-4.5 years) <br/> June 2025 Batch </label>
-                        </div>
-                        <div class="radio-option">
-                          <input type="radio" name="program" value="srkg"/>
-                          <label for="srkg"> Sr.KG (4.5-5.5 years) <br/> June 2025 Batch </label>
-                        </div>
-                      </div>
-                   </div>
-                   <div class="button-container">
-                      <button>Cancel</button>
-                      <button>Submit</button>
-                   </div>
-              </div>
+  return ` 
+      <div class="preschool-tour-semi-circle-1">
+   
+        <!-- form  -->
+       <div class="preschool-tour-form">
+        <form id="booking-form">
+          <p>Book a Preschool Tour</p>
+
+          <div class="form-field">
+            <label for="name">Name:</label>
+            <input type="text" id="name" name="name" required>
+          </div>
+          
+          <div class="form-field">
+          <label for="email">Email:</label>
+          <input type="email" id="email" name="email" required>
+        </div>
+
+          <div class="form-field">
+          <label for="mobile">Mobile:</label>
+          <input type="tel" id="mobile" name="mobile" required>
+       </div>
+
+          <div class="form-field">
+          <label for="childname">Child's Name:</label>
+          <input type="text" id="childname" name="childname" required>
+       </div>
+
+       <div class="form-field">
+          <label for="childage">Child's Age:</label>
+          <div class="age-inputs">
+            <input type="number" id="childage-year" name="childage-year" placeholder="Year" required min="0">
+            <input type="number" id="childage-month" name="childage-month" placeholder="Month" required min="0" max="11">
           </div>
         </div>
-      `;
-}
+    
+        <div class="form-field">
+          <label for="city">City:</label>
+          <input type="text" id="city" name="city" required>
+        </div>
 
+        <div class="form-field" id="submit-button">
+          <button type="submit" >Book</button>
+          </div>
+        </form>
+       </div>
+    
+    </div>`;
+}
 export {
-	renderAdmissionIntoScreen,
-	renderAdmissionFeeStructureScreen,
-	renderAdmissionProcessScreen1,
-	renderAdmissionProcessScreen2,
-	renderAdmissionRegistrationScreen1,
-	// renderAdmissionRegistrationScreen2,
+  renderAdmissionIntoScreen,
+  renderAdmissionFeeStructureScreen,
+  renderAdmissionProcessScreen1,
+  renderAdmissionProcessScreen2,
+  renderAdmissionRegistrationScreen1,
+  // renderAdmissionRegistrationScreen2,
 };
