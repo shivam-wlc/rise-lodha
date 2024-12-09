d05c9ee9686f228f7000118c52d9db3fa26b8007
shivam-wlc <shivam@wlc.technology>
2024-12-09 14:44:32 +0530

    Latest Changes

--- src/scripts/responsiveVideo.js
+++ src/scripts/responsiveVideo.js
@@ -1,17 +1,37 @@
 function updateVideoSource() {
-	const videoSource = document.getElementById("video-source");
-	const video = document.getElementById("responsive-video");
+  const videoSource = document.getElementById("video-source");
+  const video = document.getElementById("responsive-video");
 
-	if (navigator.userAgent.match(/Safari/i)) {
-		video.setAttribute("playsinline", "");
-	}
+  function loadVideo() {
+    if (navigator.userAgent.match(/Safari/i)) {
+      video.setAttribute("playsinline", "");
+    }
 
-	if (window.innerWidth <= 768) {
-		videoSource.setAttribute("src", "./src/assessts/videos/vertical.mp4");
-	} else {
-		videoSource.setAttribute("src", "./src/assessts/videos/horizontal.mp4");
-	}
+    if (window.innerWidth <= 768) {
+      videoSource.setAttribute("src", "./src/assessts/videos/vertical.mp4");
+    } else {
+      videoSource.setAttribute("src", "./src/assessts/videos/horizontal.mp4");
+    }
+    video.load();
+  }
+  window.onload = loadVideo;
+  //   const video = document.getElementById("responsive-video");
+  //   const videoSource = document.getElementById("video-source");
 
+  //   function loadVideo() {
+  //     // Dynamically set the video source based on screen width
+  //     if (window.innerWidth <= 768) {
+  //       videoSource.setAttribute("src", "./src/assessts/videos/vertical.mp4");
+  //     } else {
+  //       videoSource.setAttribute("src", "./src/assessts/videos/horizontal.mp4");
+  //     }
+
+  //     // Reload the video with the updated source
+  //     video.load();
+  //   }
+
+  //   // Set the video source after the website fully loads
+  //   window.onload = loadVideo;
 }
 
 export { updateVideoSource };
